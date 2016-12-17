4108c172c73ef155f46345f7709eaefaa914e4d7
Alexander Esp <alexjesp@gmail.com>
2016-12-17 20:59:04 +0000

    Move youtube vid to bottom and autoplay

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const compression = require('compression')
 const app = express()
 
+app.use(compression())
+app.use('/styles', express.static('public', {
+  maxAge: 0 // don't cache css
+}))
 app.use(express.static('public', {
   maxAge: 24 * 60 * 60 * 1000 // 1 day
 }))
